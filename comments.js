62294157576522190a6c57c33ab6f0d7ce791d7d
borisnikova <149790242+borisnikova@users.noreply.github.com>
2023-11-30 21:23:19 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,11 @@
+// Create web server application
+
+// Import modules
+const express = require('express');
+const bodyParser = require('body-parser');  // Parse JSON data
+
+// Create web server
+const app = express();
+
+// Set port
+const port = 3000;
